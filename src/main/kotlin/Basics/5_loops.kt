90d40f94da93de2c12db224f5f9c8a38bf22e3d8
Sumonta Saha Mridul <sumontasaha80@gmail.com>
2022-09-03 22:06:48 +0600

    loops added

---
+++ src/main/kotlin/Basics/5_loops.kt
@@ -0,0 +1,23 @@
+package Basics
+
+fun main()
+{
+    
+    var i = 0
+    while (i < 5)
+    {
+        println(i)
+        i++
+    }
+
+    // * do - while loop
+    // * oop will always be executed at least once,
+    // * even if the condition is false
+    var j = 0
+    do {
+        println(j)
+        j++
+    }
+    while (j < 5)
+
+}
